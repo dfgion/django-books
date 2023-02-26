4f3c3fc4e69d52d95ef12f137f8c781e7ecf0495
dfgion <113937888+dfgion@users.noreply.github.com>
2023-02-26 17:20:11 +0300

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -57,8 +57,8 @@ DATABASES = {
         'NAME': 'Books',
         'HOST': '127.0.0.1',
         'PORT': '5432',
-        'USER': 'postgres',
-        'PASSWORD': 'jjUUhy23',
+        'USER': '',
+        'PASSWORD': '',
     }
 }
 
